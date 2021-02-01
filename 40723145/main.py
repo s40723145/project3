136607b2c5a72a8f046186cb0b3e8f62e6587641
s40723145 <40723145@gm.nfu.edu.tw>
2021-02-02 00:11:48 +0800

    add the generating PMKS expression function

--- 40723145/main.py
+++ 40723145/main.py
@@ -1,38 +1,41 @@
 from pyslvs import *
 
-def collect_format(expr, input, graph, placement, target, cus, same):
-    {
-    'expression':       # the dot may be repositioned.
-        "M["+
-        expr +
-        "]",
-    'input': input,
-    'graph': graph,
-    'placement':placement,
-    'target':target,
-    'cus':cus,
-    'same':same
+def collect_format(expr: str,
+                               input=None,
+                               graph=None,
+                               placement=None,
+                               target=None,
+                               cus=None,
+                               same=None) -> str: 
+    format = {
+        # 'expression':("M[" + "\n" + expr + "]"),
+         'expression':("M[" + expr + "]"),
+        'input': input,
+        'graph': graph,
+        'placement':placement,
+        'target':target,
+        'cus':cus,
+        'same':same
     }
-    
-# print(type(format))
+    return format
 
 
 def expression():
-    vertice = 5
-    expr_list = []
+    str_expr = ""
     vp1 = VPoint(["ground", "L1"], 0, 0, "green", 0, 0)
     vp2 = VPoint(["ground", "L2"], 0, 0, "green", 90, 0)
     vp3 = VPoint(["L1", "L3"], 0, 0, "green", 12.92, 32.53)
     vp4 = VPoint(["L2", "L3"], 0, 0, "green", 73.28, 67.97)
     vp5 = VPoint(["L3"], 0, 0, "green", 33.3, 66.95)
     vp_list = [vp1, vp2, vp3, vp4, vp5]
-    for i in vp_list:
-        vp_expr = i.expr()
-        expr_list.append(vp_expr)
-    # print(vp_expr_list)
-    return expr_list
+    for vp in vp_list:
+        vp_expr = vp.expr()
+        # print(vp_expr)
+        # str_expr += vp_expr + "," + "\n"
+        str_expr += vp_expr + ","
+    return str_expr
     
-             
+
 def test_expr():
     vp1 = VPoint(["ground", "L1"], 0, 0, "green", 0,0)
     vp1_expr = vp1.expr()
@@ -43,5 +46,8 @@ def test_expr():
     
 if __name__ == "__main__":
     # test_expr()
-    print( expression() )
+    # print(expression())
+    expr = expression()
+    # print(collect_format(expr)['expression'])
+    print(collect_format(expr))
     
